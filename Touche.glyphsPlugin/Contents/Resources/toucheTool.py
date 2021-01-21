a6625463b5da164c73404487ee88d31cbe706a87
Nic Schumann <nic@nicschumann.co>
2021-01-21 18:50:20 -0500

    [prev/next] adds prev/next arrow callbacks to change the position in the clash list.

--- Touche.glyphsPlugin/Contents/Resources/toucheTool.py
+++ Touche.glyphsPlugin/Contents/Resources/toucheTool.py
@@ -10,7 +10,7 @@ import time, objc
 from builtins import chr
 
 class ToucheTool():
-    
+
     def __init__(self):
         NSUserDefaults.standardUserDefaults().registerDefaults_({"ToucheWindowHeight":340})
         self.windowHeight = NSUserDefaults.standardUserDefaults().integerForKey_("ToucheWindowHeight")
@@ -23,49 +23,72 @@ class ToucheTool():
         self.isResizing = False
         p = 10
         w = 160
-        
+
         # options
         self.w.options = Group((0, 0, 180, 220))
-        
+
         buttons = {
             "checkSelBtn": {"text": "Check selected glyphs", "callback": self.checkSel_, "y": p},
         }
         for button, data in buttons.items():
-            setattr(self.w.options, button, 
+            setattr(self.w.options, button,
             Button((p, data["y"], w - 22, 22), data["text"], callback=data["callback"], sizeStyle="small"))
-            
+
         self.w.options.zeroCheck = CheckBox((p, 35, w, 20), "Ignore zero-width glyphs", value=True, sizeStyle="small")
         self.w.options.progress = ProgressSpinner((w - 8, 13, 16, 16), sizeStyle="small")
-        
+
+        # Modification(Nic)
+        self.w.options.prevButton = Button((w + 22, 35, 10, 10), "Prev Button", callback=self.prevPair_)
+        self.w.options.prevButton.bind("uparrow", [])
+
+        self.w.options.nextButton = Button((w + 22, 35, 10, 10), "Next Button", callback=self.nextPair_)
+        self.w.options.nextButton.bind("downarrow", [])
+
+        # /Modification(Nic)
+
         # results
         self.w.results = Group((0, 220, 180, -0))
         self.w.results.show(False)
-        
+
         textBoxes = {"stats": -34, "result": -18}
         for box, y in textBoxes.items():
             setattr(self.w.results, box, TextBox((p, y, w, 14), "", sizeStyle="small"))
-            
-        # list and preview 
+
+        # list and preview
         self.w.outputList = List((0,58,-0,-40),
             [{"left glyph": "", "right glyph": ""}], columnDescriptions=[{"title": "left glyph", "width": 90}, {"title": "right glyph"}],
             showColumnTitles=False, allowsMultipleSelection=False, enableDelete=False, selectionCallback=self.showPair_)
         self.w.outputList._setColumnAutoresizing()
         self._resizeWindow(False)
         self.w.open()
-    
-    
+
+
     # callbacks
-    
+
+    # Modification(Nic)
+
+    def prevPair_(self, sender=None):
+        currentIndices = self.w.outputList.getSelection()
+        prevIndicies = map(lambda i: max(i - 1, 0), currentIndices)
+        self.w.outputList.setSelection(prevIndicies)
+
+    def nextPair_(self, sender=None):
+        currentIndices = self.w.outputList.getSelection()
+        nextIndices = map(lambda i: min(i + 1, len(self.w.outputList) - 1), currentIndices)
+        self.w.outputList.setSelection(nextIndices)
+
+    # /Modification(Nic)
+
     def checkAll_(self, sender=None):
         self.check_(useSelection=False)
-    
+
     def checkSel_(self, sender=None):
         self.check_(useSelection=True)
-        
+
     def check_(self, useSelection):
         self._resizeWindow(enlarge=False)
         self.checkFont(useSelection=useSelection, excludeZeroWidth=self.w.options.zeroCheck.get())
-    
+
     def showPair_(self, sender=None):
         try:
             index = sender.getSelection()[0]
@@ -87,13 +110,13 @@ class ToucheTool():
                         selection.length = 2
                         if selection.location > 0:
                             selection.location -= 1
-                    
+
                     NewString = ""
                     if LeftChar < 0xffff and RightChar < 0xffff:
                         NewString = "%s%s" % (chr(LeftChar), chr(RightChar))
                     else:
                         print("Upper plane codes are not supported yet")
-                    
+
                     textStorage.replaceCharactersInRange_withString_(selection, NewString)
                     selection.length = 0
                     selection.location += 1
@@ -145,7 +168,7 @@ class ToucheTool():
         self.isResizing = True
         self.w.setPosSize((posSize[0], posSize[1], posSize[2], targetHeight))
         self.isResizing = False
-    
+
     # ok let's do this
     @objc.python_method
     def checkFont(self, useSelection=False, excludeZeroWidth=True):
@@ -161,23 +184,23 @@ class ToucheTool():
             glyphList = [g.layers[masterID] for g in glyphs]
             glyphList = self._trimGlyphList(glyphList)
             self.touchingPairs = Touche(f, masterID).findTouchingPairs(glyphList)
-        
+
             # display output
             self.w.results.stats.set("%d glyphs checked" % len(glyphList))
             self.w.results.result.set("%d touching pairs found" % len(self.touchingPairs))
             self.w.results.show(True)
-            
+
             outputList = [{"left glyph": g1, "right glyph": g2} for (g1, g2) in self.touchingPairs]
             self.w.outputList.set(outputList)
             if len(self.touchingPairs) > 0:
                 self.w.outputList.setSelection([0])
-            
+
             #self.w.preview.setFont(f)
             self.w.options.progress.stop()
             self._resizeWindow(enlarge=True)
-        
+
             time1 = time.time()
             print('Touché: finished checking %d glyphs in %.2f seconds' % (len(glyphList), time1-time0))
-            
+
         else:
             Message('Touché: Can’t find a font to check')
